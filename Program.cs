38d20c5d69b265db904eefa12b6e9780b02d2827
anasalarby <121637668+anasalarby@users.noreply.github.com>
2022-12-29 23:20:07 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 1n");
+             int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter 2n");
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int x = start; x <= end; x++)
+            {
+                sum = 0;
+
+                for (int i = 1; i <= x / 2; i++)
+                {
+                    if (x % i == 0)
+                        sum += i;
+                       
+                }
+                if (sum == x)
+                    Console.WriteLine(x + " is a perfect number");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
